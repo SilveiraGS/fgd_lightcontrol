20e8e329e3a2e18dc7d969c8dd662541b3b52f82
Guilherme Graeff <146678912+SilveiraGS@users.noreply.github.com>
2026-04-02 13:14:53 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "Forge Group"
+description "Controlador de giroflex"
+version "1.1.8"
+
+ui_page "ui/index.html"
+
+files {
+    "ui/index.html",
+    "ui/style.css",
+    "ui/app.js",
+    "ui/images/background.png"
+}
+
+shared_scripts {
+    "config.lua"
+}
+
+client_scripts {
+    "client-side/lights_integration.lua",
+    "client-side/main.lua"
+}
+
+server_scripts {
+    "license.js",
+    "server-side/main.lua"
+}
+
+-- Removido: dependencias obrigatorias que impediam o resource de iniciar sem cnh-app/fgd_lib.
+-- Se precisar de novo, adicione aqui.
